fb6dbd44743aaf9cbc07a6e8dbc54b273922fbe9
Martin Bierbaum <martin.bierbaum@rub.de>
2017-12-12 15:47:14 +0100

    test javascript

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,15 @@
 
 from django.conf.urls import url, include
+from django.shortcuts import render
+
+from django_timer.models import Timer
+
+def index(request):
+    user = request.user if request.user.is_authenticated else None
+    timer = Timer.objects.get_or_start(user=user)
+    return render(request, 'tests/index.html', {'timer': timer})
 
 urlpatterns = [
+    url(r'^$', index, name='index'),
     url(r'', include('django_timer.urls')),
 ]
