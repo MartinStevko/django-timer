6cb628c647b90ffa0d23d56ba48825ddf26dcf19
Martin Bierbaum <martin.bierbaum@rub.de>
2017-12-13 09:47:22 +0100

    edit timer template and add csrf protection

--- tests/settings.py
+++ tests/settings.py
@@ -19,8 +19,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
