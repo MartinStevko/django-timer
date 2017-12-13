7a008d67602a845a4f235e4c78993ff834bbcc85
Martin Bierbaum <martin.bierbaum@rub.de>
2017-12-13 09:46:40 +0100

    edit test index view

--- tests/urls.py
+++ tests/urls.py
@@ -6,8 +6,11 @@ from django_timer.models import Timer
 
 def index(request):
     user = request.user if request.user.is_authenticated else None
-    timer = Timer.objects.get_or_start(user=user)
-    return render(request, 'tests/index.html', {'timer': timer})
+    try:
+        timer = Timer.objects.get_for_user(user=user)
+    except Timer.DoesNotExist:
+        timer = None
+    return render(request, 'tests/index.html', {'timer': timer, 'object_list': Timer.objects.filter(user=user, stopped=True)})
 
 urlpatterns = [
     url(r'^$', index, name='index'),
