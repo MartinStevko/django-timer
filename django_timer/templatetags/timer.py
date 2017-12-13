749121c098ffc6608b12a427419e28195acd61dd
Martin Bierbaum <martin.bierbaum@rub.de>
2017-12-13 09:46:54 +0100

    add hhmmss filter

--- django_timer/templatetags/timer.py
+++ django_timer/templatetags/timer.py
@@ -6,3 +6,12 @@ register = template.Library()
 @register.inclusion_tag('django_timer/timer.html')
 def render_timer(timer):
     return {'timer': timer}
+
+@register.filter
+def hhmmss(value):
+    minutes, seconds = divmod(round(value), 60)
+    hours, minutes = divmod(minutes, 60)
+    if hours:
+        return '{:n}:{:02n}:{:02n}'.format(hours, minutes, seconds)
+    return '{:02n}:{:02n}'.format(minutes, seconds)
+    
