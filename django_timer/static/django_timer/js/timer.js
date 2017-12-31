23624712d54feb7e8dafd5fd33a8e25c124a17be
Martin Bierbaum <martin.bierbaum@rub.de>
2017-12-31 17:15:32 +0100

    tweak timer.html css and js

--- django_timer/static/django_timer/js/timer.js
+++ django_timer/static/django_timer/js/timer.js
@@ -1,43 +1,57 @@
-function pad(number){
-    if (number < 10){
-        number = '0' + number
-    }
-    return number
+"use strict";
+
+var Timer = {
+
+    pad: function(number) {
+        if (number < 10) {
+            number = '0' + number
+        }
+        return number
+    },
+
+    printDuration: function(duration){
+        var seconds = Math.floor(duration)
+        var minutes = Math.floor(seconds/60)
+        var hours = Math.floor(minutes/60)
+        seconds = seconds % 60
+        minutes = minutes % 60
+        if (hours) {
+            return hours + ':' + Timer.pad(minutes) + ':' + Timer.pad(seconds)
+        }
+        return Timer.pad(minutes) + ':' + Timer.pad(seconds)
+    },
+
+    incrementTimer: function(){
+        Timer.duration ++
+        Timer.timer.innerHTML = Timer.printDuration(Timer.duration)
+    },
+
+    init: function(){
+        Timer.timer = document.getElementById('django-timer-display')
+        Timer.duration = Timer.timer.getAttribute('value')
+        if (Timer.timer && Timer.timer.classList.contains('active')) {
+            setInterval(Timer.incrementTimer, 1000)
+        }
+    },
 }
 
-function printDuration(duration){
-    var seconds = Math.floor(duration)
-    var minutes = Math.floor(seconds/60)
-    var hours = Math.floor(minutes/60)
-    seconds = seconds % 60
-    minutes = minutes % 60
-    if (hours) {
-        return hours + ':' + pad(minutes) + ':' + pad(seconds)
-    }
-    return pad(minutes) + ':' + pad(seconds)
-}
+Timer.init()
 
-var timer = document.getElementById('django-timer')
-
-if (timer) {
-    var duration = timer.getAttribute('value')
-    if ( duration && timer.classList.contains('active') ){
-        setInterval(function() {
-            duration++
-            timer.innerHTML = printDuration(duration)
-        }, 1000)
-    }
+var toggler = document.getElementById('django-timer-segments-toggler')
+toggler.onclick = function(){
+    var segments = document.querySelectorAll('.django-timer-segments li.inactive')
+    segments.forEach(function(segment){
+        segment.classList.toggle('hide')
+    })
 }
 
 var buttons = document.querySelectorAll('.django-timer button.ajax')
 buttons.forEach( function(element) {
-    element.addEventListener('click', 
-        function(event) {
-            event.preventDefault()
-            event.stopPropagation()
-            submitAction(element)
-        }
-    )
+    element.onclick = function(event){
+        event.preventDefault()
+        event.stopPropagation()
+        submitAction(element)
+    }
 })
 
 function submitAction(button){
@@ -48,9 +62,10 @@ function submitAction(button){
     xhttp.setRequestHeader("X-CSRFToken", csrftoken)
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4){
-            console.log(this)
             if (this.status == 200){
                 window.location.reload()
+            } else {
+                console.log(this)
             }
         }
     };
